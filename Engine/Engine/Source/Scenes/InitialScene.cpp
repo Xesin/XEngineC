1ec17872aaaa6286ec5360bfb89175212602cc3c
Francisco Ferrer Fernandez <francisco.fer.fer@gmail.com>
2017-08-03 14:14:38 +0200

    Cleaning

--- Engine/Engine/Source/Scenes/InitialScene.cpp
+++ Engine/Engine/Source/Scenes/InitialScene.cpp
@@ -14,7 +14,6 @@ Sprite* sprite;
 Rect* rect;
 
 InitialScene::InitialScene(XEngine& ref) : EngineScene(ref) {
-
 }
 
 Sprite* Mario;
@@ -56,8 +55,6 @@ void InitialScene::Start()
 	Mario->rigidBody->SetFixedRotation(true);
 	coreRef.physics->AddBoxFixture(Mario->rigidBody, Vector2(0.f, Mario->anchor.y - (Mario->frameHeight / 2.f)), Vector2(Mario->frameWidth / 2.f, Mario->frameHeight / 2.f), 0.0f, 0.0f, true);
 
-	//Transform& transform = Mario->AddComponent<Transform>();
-	//Transform& transformOut = Mario->GetComponent<Transform>();
 	//OTHER PHYSICS OBJECTS (no need to add this objects to update list or render list, there are only present in the physics engine)
 	coreRef.physics->CreateBoxBody(Vector2(40.f, 720.f / 2.f), Vector2(30.f, 720.f), 0.0f, 1.0f, PhysicBodyType::Static, false);
 	coreRef.physics->CreateBoxBody(Vector2(1230.f, 720.f / 2.f), Vector2(30.f, 720.f), 0.0f, 1.0f, PhysicBodyType::Static, false);
@@ -93,8 +90,6 @@ void InitialScene::Start()
 void InitialScene::OnDestroy() {
 	coreRef.inputManager->OnKeyDown -= newDel;
 }
-
-
 
 void InitialScene::Update(float deltaTime)
 {
